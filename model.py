a3b16d80bbcabf72806613a5d8e6a750ff2cb1f9
Ammar Rudani <aarudani7@gmail.com>
2019-12-12 23:24:18 -0500

    SVM, MultiNB and LR baseline models

--- model.py
+++ model.py
@@ -36,11 +36,17 @@ sttReviewsY = pd.read_csv('sentiment_labels.txt', sep='|',header=0).iloc[:,-1]
 # print(sttReviewsX)
 
 tTrain1 = pd.read_csv('TRECtrain1.txt', sep="\\", header=None)
-# print(tTrain1)
+tTrain2 = pd.read_csv('TRECtrain2.txt', sep="\\", header=None)
+# print(tTrain2)
 
-trecTrainX = tTrain1.iloc[:,-1]
+trecTrainX1 = tTrain1.iloc[:,-1]
 # print(trecTrainX)
-trecTrainY = tTrain1.iloc[:,0]
+trecTrainY1 = tTrain1.iloc[:,0]
+# print(trecTrainY)
+
+trecTrainX2 = tTrain2.iloc[:,-1]
+# print(trecTrainX)
+trecTrainY2 = tTrain2.iloc[:,0]
 # print(trecTrainY)
 
 # x1 = open('rtPositive.txt', "r",encoding='utf8')
@@ -61,32 +67,38 @@ trecTrainY = tTrain1.iloc[:,0]
 
 # f1 = open("reviewLabels.txt", "w")
 
-# for i in range(10662):
-#     if(i < 5331):
-#         f1.writelines('1\n')
+# for i in range(10660):
+#     if(i < 5330):
+#         f1.writelines('fresh\n')
 #     else:
-#         f1.writelines('0\n')
+#         f1.writelines('rotten\n')
 # f1.close()
 
 reviewX = open('reviewSnippets.txt', "r",encoding='utf8')
 reviewY = open('reviewLabels.txt', "r",encoding='utf8')
 # x.encode('utf8')
 
-MRx = pd.read_csv(reviewX, sep='\n', header=None, dtype='str')
+MRx = pd.read_csv(reviewX, sep='\n', header=None, dtype=str) #.values
 # MRx = [str (item) for item in MRx]
-MRy = pd.read_csv(reviewY, sep='\n', header=None, dtype='str')
-print(MRx)
+MRy = pd.read_csv(reviewY, sep='\n', header=None, dtype=str)
+# print(MRx)
+
+rX = MRx.values
+rY = MRy.iloc[::]
+# print(rY)
 
 #####Text Preprocessing techniques, using tfidf and countvectorizer
-tfidf = TfidfVectorizer(stop_words="english", lowercase=False, smooth_idf=False, sublinear_tf=True, use_idf=True, strip_accents='unicode', max_features=6550,) # ngram_range=(1,3)) #,max_df=0.1,) # max_features=65500)  
+# tfidf = TfidfVectorizer(stop_words="english", lowercase=False, smooth_idf=False, sublinear_tf=True, use_idf=True, max_features=6550,) # ngram_range=(1,3)) #,max_df=0.1,) # max_features=65500)  
 # cv = CountVectorizer(stop_words="english", analyzer='word', ngram_range=(1,2))
+tfidf = TfidfVectorizer(lowercase=False)
+cv = CountVectorizer(lowercase=False, ngram_range=(1,2))
 
 # ##### SKlearn Models created and used for training 
 lr = LogisticRegression(solver="sag", penalty="l2", class_weight="balanced")
 multiNB = MultinomialNB(alpha=0.3)
 dtc = tree.DecisionTreeClassifier()
 kf = StratifiedKFold(n_splits=5)
-svm = svm.SVC(decision_function_shape='ovr',)
+svm = svm.SVC(kernel='linear', decision_function_shape='ovo')
 
 
 # ###Helper method created to specify model being used, and to fit using said model and return a test score
@@ -98,12 +110,12 @@ def getScoretWithModel(model, x_train, x_test, y_train, y_test):
 ###################Train Test split 80/20 Using multinomial NB, fits model and runs prediction returning score##################
 #############(or any model can be inserted as the first parameter in the getscorewithmodel method)###################
 
-# x_train, x_test, y_train, y_test = train_test_split(trecTrainX, trecTrainY, test_size=0.2, random_state=4)
+x_train, x_test, y_train, y_test = train_test_split(trecTrainX2, trecTrainY2, test_size=0.2, random_state=4)
+redditDataTrainTF = cv.fit_transform(x_train)
+redditDataTestTF = cv.transform(x_test)
+print("Accuracy:", getScoretWithModel(svm, redditDataTrainTF, redditDataTestTF, y_train, y_test))
+
+# x_train, x_test, y_train, y_test = train_test_split(rX, rY, test_size=0.2, random_state=4)
 # redditDataTrainTF = cv.fit_transform(x_train)
 # redditDataTestTF = cv.transform(x_test)
 # print("Accuracy:", getScoretWithModel(multiNB, redditDataTrainTF, redditDataTestTF, y_train, y_test))
-
-x_train, x_test, y_train, y_test = train_test_split(MRx, MRy, test_size=0.2, random_state=4)
-redditDataTrainTF = tfidf.fit_transform(x_train)
-redditDataTestTF = tfidf.transform(x_test)
-print("Accuracy:", getScoretWithModel(multiNB, redditDataTrainTF, redditDataTestTF, y_train, y_test))
